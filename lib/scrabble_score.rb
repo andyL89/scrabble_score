ad7095d102fd5ede516b74d34174eb8b5e76e496
Andy Lawson <alawson89@gmail.com>
2021-02-23 10:54:25 -0800

    add passing 1st test
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,3 +1,33 @@
-def scrabble_score
+class Word
+  def initialize(word)
+    @word = word
+  end
 
+  def scrabble
+    @word.include?("a")
+    return 1
+  end
 end
+
+
+
+
+
+
+# word1 = Word.new("a")
+# puts word1.scrabble()
+
+#   def characters
+#     @characters
+#   end
+# end
+
+# def scrabble
+#   one_point = ["a"]
+#   score = 0
+#   one_point.each do |array_element|
+#     if one_point.include?("a")
+#       score = score + 1
+#     end
+#   end
+# end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,8 +1,8 @@
 require('rspec')
 require('scrabble_score')
 
-describe('scrabble_score') do
+describe('Word#scrabble') do
   it("returns a scrabble score for a letter") do
-    expect("a".scrabble()).to(eq(1))
+    expect(Word.new("a").scrabble()).to(eq(1))
   end
 end
